83d738505d4fce96765f3da67f49bba43f8bc7a8
paolotiu17 <61054234+paolotiu17@users.noreply.github.com>
2020-12-07 23:06:14 +0800

    Added Link back home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const StyledHeader = styled.header`
     display: grid;
     background-color: pink;
     padding: 1em 2em;
+    grid-template-columns: 1fr minmax(200px, 2fr);
+    align-items: center;
 `;
 
 export const Header: React.FC = () => {
     return (
         <StyledHeader>
-            <span>Blog</span>
+            <Link
+                to="/blogs"
+                style={{ textDecoration: 'none', color: 'inherit' }}
+            >
+                Blogs
+            </Link>
         </StyledHeader>
     );
 };
