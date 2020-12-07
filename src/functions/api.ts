a9d16cb287a003c1f5dd0cb4364914876feba3ff
paolotiu17 <61054234+paolotiu17@users.noreply.github.com>
2020-12-07 23:06:59 +0800

    Added fetchbyID

--- src/functions/api.ts
+++ src/functions/api.ts
@@ -2,3 +2,30 @@ export const fetchAllBlogs = async () => {
     const blogs = await fetch('https://blog-api-pt.herokuapp.com/blogs');
     return blogs.json();
 };
+
+export const fetchBlogByID = async (id: string) => {
+    const blog = await fetch('https://blog-api-pt.herokuapp.com/blogs/' + id);
+    return blog.json();
+};
+
+// export const loginUser = async (
+//     username: string = 'aaa',
+//     password: string = 'password'
+// ) => {
+//     const headers = new Headers();
+//     headers.append('Content-Type', 'application/x-www-form-urlencoded');
+
+//     const urlencoded = new URLSearchParams();
+//     urlencoded.append('username', 'aaa');
+//     urlencoded.append('password', 'password');
+
+//     const requestOptions: RequestInit = {
+//         method: 'POST',
+//         headers: headers,
+//         body: urlencoded,
+//     };
+
+//     const res = await fetch('http://localhost:3001/user/login', requestOptions);
+//     const json = await res.json();
+//     console.log(json);
+// };
