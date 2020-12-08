9f3643ee82e23e20f52389f8fbd8f07c1d264bec
paolotiu17 <61054234+paolotiu17@users.noreply.github.com>
2020-12-08 15:56:34 +0800

    added themes and commentform

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -4,6 +4,7 @@ import { formatDate } from '../../functions/customHooks';
 import { useParams } from 'react-router-dom';
 import { IBlog } from '../../types';
 import { Spinner } from '../Spinner/Spinner';
+import { CommentForm } from './CommentForm/CommentForm';
 import styled from 'styled-components';
 const Container = styled.section`
     padding: 1em;
@@ -13,12 +14,14 @@ const Container = styled.section`
     gap: 20px;
 `;
 const Title = styled.h1`
+    color: ${(props) => props.theme.headline};
     font-size: 2em;
     margin: 0;
 `;
 const Text = styled.p`
+    color: ${(props) => props.theme.paragraph};
     font-weight: 300;
-    line-height: 1.2em;
+    line-height: 1.5em;
 `;
 
 const Info = styled.div`
@@ -48,6 +51,10 @@ export const Blog: React.FC = () => {
                         <span>By: {blog.author.username}</span>
                         <span>{formatDate(blog.timestamp)}</span>
                     </Info>
+                    <CommentForm />
+                    {blog.comments.map((comment) => {
+                        return <p>{comment.author}</p>;
+                    })}
                 </Container>
             ) : (
                 <Spinner />
