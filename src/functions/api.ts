fe910d0c1399d2542385076eecbd14887d4de1bd
paolotiu17 <61054234+paolotiu17@users.noreply.github.com>
2020-12-12 00:10:20 +0800

    added blog delete and blog update

--- src/functions/api.ts
+++ src/functions/api.ts
@@ -1,10 +1,12 @@
+const url = 'https://blog-api-pt.herokuapp.com';
+
 export const fetchAllBlogs = async () => {
-    const blogs = await fetch('https://blog-api-pt.herokuapp.com/blogs');
+    const blogs = await fetch(url + '/blogs');
     return blogs.json();
 };
 
 export const fetchBlogByID = async (id: string) => {
-    const blog = await fetch('https://blog-api-pt.herokuapp.com/blogs/' + id);
+    const blog = await fetch(url + '/blogs/' + id);
     return blog.json();
 };
 
@@ -12,17 +14,11 @@ export const postComment = async (id: string, author: string, text: string) => {
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
 
-    const urlencoded = new URLSearchParams();
-    urlencoded.append('author', author);
-    urlencoded.append('text', text);
-    const res = await fetch(
-        'https://blog-api-pt.herokuapp.com/blogs/' + id + '/comment',
-        {
-            method: 'POST',
-            headers: headers,
-            body: JSON.stringify({ author, text }),
-        }
-    );
+    const res = await fetch(url + '/blogs/' + id + '/comment', {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify({ author, text }),
+    });
     return res.json();
 };
 
@@ -41,10 +37,7 @@ export const loginUser = async (username: string, password: string) => {
     };
 
     try {
-        const res = await fetch(
-            'http://localhost:3001/user/login',
-            requestOptions
-        );
+        const res = await fetch(url + '/user/login', requestOptions);
         const json = await res.json();
         return json;
     } catch (err) {
@@ -57,7 +50,7 @@ export const getUser = async () => {
     if (!token) {
         return { error: 'No token' };
     }
-    const res = await fetch('http://localhost:3001/user', {
+    const res = await fetch(url + '/user', {
         headers: {
             Authorization: 'Bearer ' + token,
         },
@@ -73,7 +66,45 @@ export const getOwnBlogs = async () => {
         return { error: 'No token' };
     }
 
-    const res = await fetch('http://localhost:3001/user/blogs', {
+    const res = await fetch(url + '/user/blogs', {
+        headers: {
+            Authorization: 'Bearer ' + token,
+        },
+    });
+
+    const json = await res.json();
+
+    return json;
+};
+
+export const updateBlog = async (id: string, title: string, text: string) => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+        return { error: 'No token' };
+    }
+
+    const res = await fetch(url + '/blogs/' + id, {
+        method: 'PUT',
+        headers: {
+            Authorization: 'Bearer ' + token,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ title, text }),
+    });
+
+    const json = await res.json();
+
+    return json;
+};
+
+export const deleteBlog = async (id: string) => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+        return { error: 'No token' };
+    }
+
+    const res = await fetch(url + '/blogs/' + id, {
+        method: 'DELETE',
         headers: {
             Authorization: 'Bearer ' + token,
         },
