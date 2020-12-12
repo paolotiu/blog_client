2e50a727223fa517b12bfc0316096b471bb02a24
paolotiu17 <61054234+paolotiu17@users.noreply.github.com>
2020-12-12 13:48:00 +0800

    Made optional modal and message props

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,19 +1,30 @@
 import styled from 'styled-components';
 import React from 'react';
-const StyledContainer = styled.div`
+const StyledContainer = styled.div<{ marginTop?: string }>`
     width: 40px;
     height: 40px;
 
     position: relative;
-    margin: 100px auto;
+    margin: ${(props) => (props.marginTop ? props.marginTop : '100px')} auto;
     p {
         position: absolute;
-        width: 250px;
-        top: 50px;
-        left: -90px;
+        top: 100%;
+        left: 50%;
+        transform: translate(-50%, 0.8em);
+        white-space: nowrap;
     }
 `;
 
+const StyledModal = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 10;
+    width: 100vw;
+    height: 100vw;
+    background-color: rgb(0, 0, 0); /* Fallback color */
+    background-color: rgba(0, 0, 0, 0.4); /* Black w/ opacity */
+`;
 const DB1 = styled.div`
     width: 100%;
     height: 100%;
@@ -54,15 +65,42 @@ const DB2 = styled(DB1)`
     -webkit-animation-delay: -1s;
     animation-delay: -1s;
 `;
+interface Props {
+    message?: string;
+    isModal?: boolean;
+    isOpen?: boolean;
+    marginTop?: string;
+}
 
-export const Spinner: React.FC = () => {
+export const Spinner: React.FC<Props> = ({
+    message = 'This may take a few seonds',
+    isModal = false,
+    isOpen = true,
+    marginTop = '100px',
+}) => {
     return (
         <>
-            <StyledContainer>
-                <DB1 />
-                <DB2 />
-                <p>This may take a few seconds</p>
-            </StyledContainer>
+            {isOpen ? (
+                <>
+                    {isModal ? (
+                        <StyledModal>
+                            <StyledContainer marginTop={marginTop}>
+                                <DB1 />
+                                <DB2 />
+                                <p>{message}</p>
+                            </StyledContainer>
+                        </StyledModal>
+                    ) : (
+                        <StyledContainer>
+                            <DB1 />
+                            <DB2 />
+                            <p>{message}</p>
+                        </StyledContainer>
+                    )}
+                </>
+            ) : (
+                ''
+            )}
         </>
     );
 };
